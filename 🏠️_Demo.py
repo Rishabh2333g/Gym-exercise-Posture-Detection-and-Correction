df57c038862a13d0d13b60e75f911d92b59c2660
Rishabh Gupta <156583510+Rishabh2333g@users.noreply.github.com>
2024-04-16 22:01:11 +0530

    Update 🏠️_Demo.py

--- 🏠️_Demo.py
+++ 🏠️_Demo.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title('Fitness Trainer: Squats Analysis (R&D Project)')
 
 
-recorded_file = 'output_sample.mp4'
+recorded_file = 'output_sample1.mp4'
 sample_vid = st.empty()
 sample_vid.video(recorded_file)
 
